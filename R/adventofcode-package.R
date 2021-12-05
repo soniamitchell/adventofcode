73169cf7f806f496b0f9b601477b4894cc17983a
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-12-05 20:04:28 +0000

    update namespace

---
+++ R/adventofcode-package.R
@@ -0,0 +1,13 @@
+#' adventofcode
+#'
+#' adventofcode solutions in R
+#'
+#' @references \url{https://adventofcode.com/2021}
+#' @keywords internal
+#'
+#' @import dplyr
+#' @import here
+#'
+"_PACKAGE"
+
+NULL
