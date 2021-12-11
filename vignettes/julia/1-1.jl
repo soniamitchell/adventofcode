2c8a1f131fde3c7c42baf42ecf381d7e07faad85
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-12-11 18:59:21 +0000

    use `prod()`

--- vignettes/julia/1-1.jl
+++ vignettes/julia/1-1.jl
@@ -7,4 +7,4 @@ numbers = [x for x in combinations(dat, 2) if sum(x) == 2020];
 numbers = reshape(numbers)[1];
 
 # What do you get if you multiply them together?
-numbers[1] * numbers[2]
+prod(numbers)
