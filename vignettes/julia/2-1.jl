52819b4e5fc75d02808a0c42a0eb586440a61a22
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-12-11 18:06:44 +0000

    fix scoping

--- vignettes/julia/2-1.jl
+++ vignettes/julia/2-1.jl
@@ -2,16 +2,19 @@
 path = joinpath(here, "inst", "2020", "day2.txt");
 dat = readlines(path);
 
-counter = 0
-for i in dat
-    m = match(r"(\d+)-(\d+)\s+(.):\s+(.+)", i)  # grep
-    minimum = parse(Int64, m.captures[1])       # convert to Int64
-    maximum = parse(Int64, m.captures[2])
-    letter = m.captures[3]
-    password = m.captures[4]
-    n = count(letter, password)
-    counter += (n >= minimum && n <= maximum) ? 1 : 0
-end
+function sled_password_check(dat)
+    results = 0
+    for i in dat
+        m = match(r"(\d+)-(\d+)\s+(.):\s+(.+)", i)  # grep
+        minimum = parse(Int64, m.captures[1])       # convert to Int64
+        maximum = parse(Int64, m.captures[2])
+        letter = m.captures[3]
+        password = m.captures[4]
+        n = count(letter, password)
+        results += (n >= minimum && n <= maximum) ? 1 : 0
+    end
+    results
+end;
 
 # How many passwords are valid according to their policies?
-counter
+sled_password_check(dat)
