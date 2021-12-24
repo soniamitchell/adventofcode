9c958f752317ac6fbce358835f6ec1ae7f69c250
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-12-24 17:49:13 +0000

    unfinished

--- vignettes/julia/5-1.jl
+++ vignettes/julia/5-1.jl
@@ -9,21 +9,21 @@ function seat(dat)
         # Get row
         row_directions = dat[i][1:7]
         for j in row_directions
-            if j == only("F")
-                row_max = floor(row_max - (row_max - row_min) / 2)
-                row_min = row_min
+#            if j == only("F")
+ #               row_max = floor(row_max - (row_max - row_min) / 2)
+#                row_min = row_min
 
-            elseif j == only("B")
-                row_max = row_max
-                row_min = round(row_min + (row_max - row_min) / 2)
-            end
-            return row_min
+#            elseif j == only("B")
+#                row_max = row_max
+#                row_min = round(row_min + (row_max - row_min) / 2)
+#            end
+#            return row_min
         end
 
         # Get seat
-        seat = dat[i][8:10]
-        for j in seat
-        end
+#        seat = dat[i][8:10]
+#        for j in seat
+ #       end
     end
 end
 
