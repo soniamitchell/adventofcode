f972303d8fa9c243b276e5ff4ad31bbe93551485
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2022-01-01 15:15:16 +0000

    2020 day 5

--- vignettes/julia/src/5-1.jl
+++ vignettes/julia/src/5-1.jl
@@ -2,29 +2,47 @@
 path = joinpath("..", "inst", "2020", "day5.txt");
 dat = readlines(path);
 
-row_min, row_max = 0, 127
+function findseat(dat)
+   
+    output = []
 
-function seat(dat)
     for i in eachindex(dat)
+        
+        row_min, row_max = 0, 127
+        col_min, col_max = 0, 7
+
         # Get row
         row_directions = dat[i][1:7]
         for j in row_directions
-#            if j == only("F")
- #               row_max = floor(row_max - (row_max - row_min) / 2)
-#                row_min = row_min
-
-#            elseif j == only("B")
-#                row_max = row_max
-#                row_min = round(row_min + (row_max - row_min) / 2)
-#            end
-#            return row_min
+            if j == only("F")
+                row_max = floor(Int, row_max - (row_max - row_min) / 2)
+            elseif j == only("B")
+                row_min = ceil(Int, row_min + (row_max - row_min) / 2)
+            end
         end
 
-        # Get seat
-#        seat = dat[i][8:10]
-#        for j in seat
- #       end
-    end
-end
+        # Get column
+        seat = dat[i][8:10]
+        for j in seat
+            if j == only("L")
+                col_max = floor(Int, col_max - (col_max - col_min) / 2)
+            elseif j == only("R")
+                col_min = ceil(Int, col_min + (col_max - col_min) / 2)
+            end
+        end
 
-seat(dat)
+    # Calculate seat ID
+    seat_id = (row_min * 8) + col_max
+    push!(output, seat_id)
+    end
+
+    output
+end;
+
+# Try test data
+test = readlines(joinpath("..", "inst", "2020", "day5-test.txt"));
+@test all(findseat(test) .== [357, 567, 119, 820])
+
+# What is the highest seat ID on a boarding pass?
+seat_ids = findseat(dat)
+maximum(seat_ids)
