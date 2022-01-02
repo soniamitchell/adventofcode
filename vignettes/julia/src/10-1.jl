606c306e66331b90b4e35445983a1630ba85775d
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2022-01-02 21:55:06 +0000

    refactor

--- vignettes/julia/src/10-1.jl
+++ vignettes/julia/src/10-1.jl
@@ -1,45 +1,21 @@
-# Read in data ------------------------------------------------------------
-
-path = joinpath("..", "inst", "2020", "day10.txt");
-dat = readlines(path);
-dat = parse.(Int64, dat);                       # convert string to numeric
-
-# Define functions --------------------------------------------------------
-
-function chain(dat)
-    current = 0
-    log = []
-    target = length(dat)
-
-    while length(log) != target
-        # find index of next adapter
-        possible = collect(current .+ (1:3))    # possible values to match against         
-        index = indexin(possible, dat)          # find index of matching values       
-        filter!(x -> x != nothing, index)       # remove `nothing`    
-        index = index[1]                        # use first index (lowest value)
-
-
-        value = dat[index]
-        # store difference
-        push!(log, value - current)
-
-        # set current value
-        current = copy(value)
-   
-        # remove value from `dat`
-        deleteat!(dat, index)
-    end
-
-    push!(log, 3)
-    log
-end;
+path = joinpath(".", "inst", "2020", "day10.txt")
 
 # Find a chain that uses all of your adapters to connect the charging outlet to your 
 # device's built-in adapter and count the joltage differences between the charging 
 # outlet, the adapters, and your device
 
-res = chain(dat);
+function day10(path)
+    dat = readlines(path)                           # read data
+    dat = parse.(Int64, dat)                        # convert string to integer
+end
+
+function part1(dat)
+    tmp = copy(dat) |> sort                         # sort values
+    whole = vcat(0, tmp, tmp[end] + 3)              # prepend with 0, append with +3
+    ones = count(==(1), diff(sort(whole)))          # count number of ones
+    threes = count(==(3), diff(sort(whole)))        # count number of threes
+    ones * threes
+end
 
 # What is the number of 1-jolt differences multiplied by the number of 3-jolt differences?
-
-sum(res .== 1) * sum(res .== 3)
+day10(path) |> part1
