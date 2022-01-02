5aa567a7b3ff46a7d37b8e3df9e4bdb531393d93
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2022-01-02 02:06:29 +0000

    tidy up

--- vignettes/julia/src/8-1.jl
+++ vignettes/julia/src/8-1.jl
@@ -5,35 +5,40 @@ dat = filter(!isempty, dat);                # remove empty last element
 dat = split.(dat, " ");                     # split by space
 
 # Tidy up data
-inst = [x[1] for x in dat]                  # extract instructions
-val = [x[2] for x in dat]                   # extract values
+inst = [x[1] for x in dat];                 # extract instructions
+val = [x[2] for x in dat];                  # extract values
 
 vals = map(val) do str
     mat = match.(r"[\+|\-](\d*)", str)      # regex to extract number
     cap = mat.captures                      # extract capture group
     num = parse(Int64, cap[1])              # convert string to number
     contains.(str, "-") ? num * -1 : num    # multiply by -1 if negative
-end
+end;
 
 # Run your copy of the boot code
 
-accumulator = 0                             # initialise accumulator
-i = 1                                       # initialise place in instruction list
-log = []
+function boot(inst, vals)
 
-while i ∉ log                               # stop before an instruction is run again
-    push!(log, i)                           # record i in log
+    accumulator = 0                         # initialise accumulator
+    i = 1                                   # initialise place in instruction list
+    log = []
 
-    if inst[i] == "acc"
-        accumulator += vals[i]              # add value to accumulator
-        i += 1                              # next instruction
-    elseif inst[i] == "jmp"
-        i += vals[i]                        # jump to a new instruction
-    elseif inst[i] == "nop"
-        i += 1                              # next instruction
+    while i ∉ log                           # stop before an instruction is run again
+        push!(log, i)                       # record i in log
+
+        if inst[i] == "acc"
+            accumulator += vals[i]          # add value to accumulator
+            i += 1                          # next instruction
+        elseif inst[i] == "jmp"
+            i += vals[i]                    # jump to a new instruction
+        elseif inst[i] == "nop"
+            i += 1                          # next instruction
+        end
     end
-end
+
+    accumulator
+end;
 
 # Immediately before any instruction is executed a second time, what value is in the 
 # accumulator?
-accumulator
+boot(inst, vals)
