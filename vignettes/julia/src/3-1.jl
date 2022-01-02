141837581da3a615ef6f25fc71a8aad5fec736d6
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2022-01-02 03:31:05 +0000

    tidy up

--- vignettes/julia/src/3-1.jl
+++ vignettes/julia/src/3-1.jl
@@ -20,6 +20,6 @@ function trees(x, y, map)
     
 end;
 
-# Starting at the top-left corner of your map and following a slope of right 3 and down 1, 
-# how many trees would you encounter?
+# Starting at the top-left corner of your map and following a slope of right 3 and 
+# down 1, how many trees would you encounter?
 trees(3, 1, dat)
