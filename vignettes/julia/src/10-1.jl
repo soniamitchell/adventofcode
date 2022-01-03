de6c9a2e7f69e6ac109003d5a01b75fbb48b7107
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2022-01-03 00:57:49 +0000

    tidy up

--- vignettes/julia/src/10-1.jl
+++ vignettes/julia/src/10-1.jl
@@ -1,4 +1,4 @@
-path = joinpath("..", "inst", "2020", "day10.txt")
+path = joinpath("..", "inst", "2020", "day10.txt");
 
 # Find a chain that uses all of your adapters to connect the charging outlet to your 
 # device's built-in adapter and count the joltage differences between the charging 
@@ -7,7 +7,7 @@ path = joinpath("..", "inst", "2020", "day10.txt")
 function day10(path)
     dat = readlines(path)                           # read data
     dat = parse.(Int64, dat)                        # convert string to integer
-end
+end;
 
 function part1(dat)
     tmp = copy(dat) |> sort                         # sort values
@@ -15,7 +15,7 @@ function part1(dat)
     ones = count(==(1), diff(sort(whole)))          # count number of ones
     threes = count(==(3), diff(sort(whole)))        # count number of threes
     ones * threes
-end
+end;
 
 # What is the number of 1-jolt differences multiplied by the number of 3-jolt 
 # differences?
