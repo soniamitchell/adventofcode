6f8de415de2fc09a6f6c590472e77553b17093e0
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2022-01-03 18:59:43 +0000

    2021 day 14

--- vignettes/2021/R/14-1.R
+++ vignettes/2021/R/14-1.R
@@ -1,40 +1,53 @@
+# Define functions --------------------------------------------------------
+
+get_template <- function(path) {
+  strsplit(readLines(path, n = 1), "") |>
+    unlist()
+}
+
+convert_template <- function(template) {
+  data.frame(first = head(template, -1),
+             last = tail(template, -1))
+}
+
+get_rules <- function(path) {
+  tmp <- read.table(path, skip = 1)
+  data.frame(first  = substr(tmp[, 1], 1, 1),
+             last = substr(tmp[, 1], 2, 2),
+             insert = tmp[, 3])
+}
+
+polymerization <- function(template, rules, n) {
+  for (i in seq_len(n)) {
+    tmp <- convert_template(template) |>
+      dplyr::left_join(rules, by = c("first", "last"))
+    template <- tmp |> dplyr::select(insert, last) |>
+      t() |>
+      as.vector() |>
+      append(tmp$first[1], 0)
+  }
+
+  freq <- table(template)
+  max(freq) - min(freq)
+}
+
 # Read in data ------------------------------------------------------------
 
+test <- here("inst", "2021", "day14-test.txt")
+path <- here("inst", "2021", "day14.txt")
+
 # Read in polymer template
-path <- here("inst", "2021", "day14-test.txt")
-template <- path |>
-  scan(what = "character", n = 1)
+test_template <- get_template(test)
+template <- get_template(path)
 
 # Read in pair insertion rules
-rules <- path |>
-  scan(what = "character", sep = "\n", skip = 2) |>
-  data.frame() |>
-  setNames("x") |>
-  tidyr::separate(x, c("pair", "insert"), " -> ")
+test_rules <- get_rules(test)
+rules <- get_rules(path)
 
 # Run simulation ----------------------------------------------------------
 
-current_string <- strsplit(template, "")[[1]]
-
-# Apply 10 steps of pair insertion to the polymer template and find the most
-# and least common elements in the result
-for (i in seq_len(10)) {
-  next_string <- ""
-  # cat("\r", i)
-  for (j in seq_len(length(current_string) - 1)) {
-    index <- which(rules$pair == paste(current_string[j:(j + 1)],
-                                       collapse = ""))
-    if (length(index) != 0) {
-      next_string <- paste0(next_string, current_string[j], rules$insert[index])
-    } else {
-      next_string <- paste0(next_string, current_string[j])
-    }
-  }
-  next_string <- paste0(next_string, tail(current_string, 1))
-  current_string <- strsplit(next_string, "")[[1]]
-}
+assertthat::assert_that(polymerization(test_template, test_rules, 10) == 1588)
 
 # What do you get if you take the quantity of the most common element and
 # subtract the quantity of the least common element?
-freq <- table(current_string)
-max(freq) - min(freq)
+polymerization(template, rules, 10)
--- vignettes/2021/R/14-2.R
+++ vignettes/2021/R/14-2.R
@@ -1,38 +1,44 @@
-# Apply 40 steps of pair insertion to the polymer template and find the most
-# and least common elements in the result. What do you get if you take the
-# quantity of the most common element and subtract the quantity of the least
-# common element?
-current_string <- strsplit(template, "")[[1]]
-iterations <- 10
+# Define functions --------------------------------------------------------
 
-generate_pairs <- function(x) {
-  tmp <- do.call(cbind, data.table::shift(x, 0:1, type = "lead")) |>
-    head(-1)
-  lapply(seq_len(nrow(tmp)), function(i) tmp[i, ])
-}
+polymerization2 <- function(template, rules, n) {
+  first <- template[1]
+  template <- convert_template(template) |>
+    dplyr::mutate(count = 1)
 
-new_rules <- tidyr::separate(rules, pair, c("first", "last"), "(?<=[A-Z])")
+  for (i in seq_len(n)) {
+    tmp <- dplyr::left_join(template, rules, by = c("first", "last"))
 
-for (i in seq_len(iterations)) {
-  cat("\r", i)
-  # Generate list of vectors of overlapping pairs
-  pairs <- generate_pairs(current_string) |>
-    do.call(what = rbind) |>
-    data.frame()
+    set1 <- tmp |> dplyr::select(-last) |> dplyr::rename(last = insert)
+    set2 <- tmp |> dplyr::select(-first) |> dplyr::rename(first = insert)
+    pairs <- rbind(set1, set2)
 
-  for (j in seq_len(length(pairs))) {
-    this_pair <- pairs[[j]]
+    counts <- pairs |>
+      dplyr::group_by(first, last) |>
+      dplyr::summarise(count = sum(count), .groups = "drop")
 
-    # If there's an insertion, insert it back into `pairs`
-    insertion <- dplyr::filter(new_rules, first == this_pair[1] &
-                                 last == this_pair[2])
-    if (nrow(insertion) > 0)
-      pairs[[j]] <- append(this_pair, insertion$insert, 1)
+    template <- counts
   }
 
-  # Flatten list
-  current_string <- lapply(pairs, function(x) head(x, -1)) |> unlist()
+  # Final count
+  total <- template |>
+    dplyr::select(-first) |>
+    dplyr::group_by(last) |>
+    dplyr::summarise(count = sum(count)) |>
+    dplyr::mutate(count = dplyr::case_when(last == first ~ count + 1,
+                                           TRUE ~ count))
+
+  max(total$count) - min(total$count)
 }
 
-freq <- table(current_string)
-max(freq) - min(freq)
+# Run simulation ----------------------------------------------------------
+
+# Apply 40 steps of pair insertion to the polymer template and find the
+# most and least common elements in the result
+
+assertthat::assert_that(
+  polymerization2(test_template, test_rules, 40) == 2188189693529)
+
+# What do you get if you take the quantity of the most common element
+# and subtract the quantity of the least common element?
+
+polymerization2(template, rules, 40) |> format(scientific = FALSE)
