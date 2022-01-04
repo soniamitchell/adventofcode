0eeb18cd7fee80be29a4807cc390c03646376f7b
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2022-01-04 13:08:19 +0000

    tidy up

--- vignettes/2021/R/11-1.R
+++ vignettes/2021/R/11-1.R
@@ -13,10 +13,12 @@ find_neighbours <- function(x, y, flashing_now, flashed_previously, octopus) {
   tmp <- rbind(data.frame(row = x - 1, col = (y - 1):(y + 1)), # above
                data.frame(row = x, col = c(y - 1, y + 1)),     # sides
                data.frame(row = x + 1, col = (y - 1):(y + 1))) # below
+
   # Remove invalid coordinates
   xlim <- ncol(octopus) + 1
   ylim <- nrow(octopus) + 1
   tmp <- dplyr::filter(tmp, row != 0, col != 0, row != xlim, col != ylim)
+
   # Add to `neighbours`, remove duplicates, and remove those that are flashing
   # now or have flashed previously
   tmp |>
@@ -31,28 +33,24 @@ timestep <- function(octopus) {
   octopus <- octopus + 1
   done <- matrix(nrow = 0, ncol = 2)
 
-  # Any octopus with an energy level greater than 9 flashes and has its energy
-  # level reset to 0, the energy level of all adjacent octopuses (including
-  # octopuses that are diagonally adjacent) increases by 1. If this causes an
-  # octopus to have an energy level greater than 9, it also flashes
+  # Octopi with energy levels > 9 flash and their energy levels reset to 0,
+  # the energy level of all adjacent octopuses increases by 1. If this causes
+  # an octopus to have an energy level > 9, it also flashes
   while (any(octopus > 9)) {
-    # Check if energy level is greater than 9
-    flash <- which(octopus > 9, arr.ind = T)
-    # Flash
-    done <- rbind(done, flash)
-    # Reset energy
-    octopus[flash] <- 0
+    flash <- which(octopus > 9, arr.ind = T)    # if energy is > 9
+    done <- rbind(done, flash)                  # flash
+    octopus[flash] <- 0                         # and reset energy level
 
-    # For each flashing octopus
+    # For each flashing octopus, find neighbouring octopi, and increase their
+    # energy levels by 1
     for (j in seq_len(nrow(flash))) {
       x <- unname(flash[j, "row"])
       y <- unname(flash[j, "col"])
-      # Find adjacent octopi
       neighbours <- find_neighbours(x, y, flash, done, octopus)
-      # Increase energy level by 1
       octopus[neighbours] <- octopus[neighbours] + 1
     }
   }
+
   # Latest flashes
   list(octopus = octopus, latest_flashes = nrow(done))
 }
